76bd89cc4c33db01cdb10de497c98d179e4314be
Oranos-Dragon <302164886@student.rocmondriaan.nl>
2022-11-17 14:00:02 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let box1 = 0;
+let box2 = 0;
+let box3 = 0;
+let box4 = 0;
+let box5 = 0;
+let box6 = 0;
+let box7 = 0;
+let box8 = 0;
+let box9 = 0;
+
+const bx1 = document.querySelector(".box1")
+
+let playerTurn = 1
+
+function box1() {
+    if (playerTurn == 1) {
+        Element.querySelector(".box1");
+        
+    }
+}
